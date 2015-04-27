28b500a85995d6590bdb70f2bc82fdc54fc5c5b4
Philipp Paulweber <p.paulweber@gmail.com>
2015-04-27 23:50:54 +0200

    AST Dumper
    * removed 'throw' in unimplemented AST nodes and replaced them
      with simple printf's to continue with AST dumping

--- src/libsyntax/ast_dump_visitor.h
+++ src/libsyntax/ast_dump_visitor.h
@@ -11,20 +11,20 @@
 class AstDumpVisitor : public BaseVisitor<bool> {
   private:
     std::stringstream dump_stream_;
-
+    
     void dump_node(uint64_t key, const std::string& name);
     void dump_node(AstNode *n, const std::string& name);
     void dump_link(uint64_t from, uint64_t to);
     void dump_link(AstNode *from, AstNode *to);
-
-
+    
+    
   public:
     bool arguments[10];
     uint32_t num_arguments;
-
+    
     AstDumpVisitor();
     std::string get_dump();
-
+    
     void visit_init(AstNode *init);
     void visit_body_elements(AstListNode *body_elements);
     void visit_function_def(FunctionDefNode *def, const std::vector<std::pair<bool, bool>>&);
@@ -47,10 +47,10 @@ class AstDumpVisitor : public BaseVisitor<bool> {
     void visit_let(LetNode*, bool);
     void visit_pop(PopNode*);
     void visit_push(PushNode*, bool, bool);
-    void visit_case(CaseNode*, const bool, const std::vector<bool>&) { throw "not implemented case astdump"; }
-
-    void visit_forall_post(ForallNode*) { throw "not implemented"; }
-    void visit_iterate(UnaryNode*) { throw "not implemented"; }
+    void visit_case(CaseNode*, const bool, const std::vector<bool>&) { fprintf( stderr, "%s: %s: not implemented\n", __FILE__, __FUNCTION__ ); }
+            
+            void visit_forall_post(ForallNode*) { fprintf( stderr, "%s: %s: not implemented\n", __FILE__, __FUNCTION__ ); }
+        void visit_iterate(UnaryNode*) { fprintf( stderr, "%s: %s: not implemented\n", __FILE__, __FUNCTION__ ); }
 
     bool visit_expression(Expression *expr, bool, bool);
     bool visit_expression_single(Expression *expr, bool);
@@ -59,13 +59,13 @@ class AstDumpVisitor : public BaseVisitor<bool> {
     bool visit_undef_atom(UndefAtom*);
     bool visit_function_atom(FunctionAtom *atom, bool[], uint16_t);
     bool visit_builtin_atom(BuiltinAtom *, bool[], uint16_t);
-    bool visit_derived_function_atom(FunctionAtom*, bool) { throw "not implemented"; }
+    bool visit_derived_function_atom(FunctionAtom*, bool) { fprintf( stderr, "%s: %s: not implemented\n", __FILE__, __FUNCTION__ ); return 0; }
     bool visit_self_atom(SelfAtom *atom);
     bool visit_rule_atom(RuleAtom *atom);
     bool visit_boolean_atom(BooleanAtom *atom);
     bool visit_string_atom(StringAtom *atom);
     bool visit_list_atom(ListAtom*, std::vector<bool>&);
-    bool visit_number_range_atom(NumberRangeAtom*) { throw "not implemented"; }
+    bool visit_number_range_atom(NumberRangeAtom*) { fprintf( stderr, "%s: %s: not implemented\n", __FILE__, __FUNCTION__ ); return 0; }
 };
 
 #endif //CASMI_LIBINTERPRETER_EXEC_VISITOR
