bd54efb1a095c19630cc2fb2e9c45d2d6f360f1e
Philipp Paulweber <p.paulweber@gmail.com>
2016-08-25 08:47:55 +0200

    Repository
    * removed 'old' source files
    * removed 'old' benchmarks

--- stash/src/libmiddle/function_cycle_visitor.cpp
+++
@@ -1,6 +0,0 @@
-#include "libmiddle/function_cycle_visitor.h"
-
-bool InitCycleVisitor::visit_function_atom(FunctionAtom *atom, bool[], uint16_t) {
-  dependency_names.insert(atom->name);
-  return true;
-}
--- stash/src/libsyntax/parser.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include <stdio.h>
-
-int parse(FILE* f);
-
-#endif
