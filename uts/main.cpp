8abd0ce1f00c71b4a50e2c2ba1433ec4c8f482f5
Philipp Paulweber <p.paulweber@gmail.com>
2017-05-04 16:06:44 +0200

    Repository
    * fixed license information

--- uts/main.cpp
+++ uts/main.cpp
@@ -1,24 +1,26 @@
 //
-//  Copyright (c) 2015-2017 CASM Organization
+//  Copyright (c) 2014-2017 CASM Organization
 //  All rights reserved.
 //
 //  Developed by: Philipp Paulweber
-//                https://github.com/casm-lang/libcasm-ir
+//                Emmanuel Pescosta
+//                Florian Hahn
+//                https://github.com/casm-lang/libcasm-fe
 //
-//  This file is part of libcasm-ir.
+//  This file is part of libcasm-fe.
 //
-//  libcasm-ir is free software: you can redistribute it and/or modify
+//  libcasm-fe is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
 //
-//  libcasm-ir is distributed in the hope that it will be useful,
+//  libcasm-fe is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with libcasm-ir. If not, see <http://www.gnu.org/licenses/>.
+//  along with libcasm-fe. If not, see <http://www.gnu.org/licenses/>.
 //
 
 #include "uts/main.h"
