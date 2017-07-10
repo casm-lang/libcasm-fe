5762f8bc60a00ee7b936656d7d6bb0dd483fcfbb
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2017-07-10 13:13:43 +0200

    Fix build with clang 3.9 (missing FunctionDefinition::UID hash specialization)

--- src/ast/Definition.h
+++ src/ast/Definition.h
@@ -219,6 +219,18 @@ namespace libcasm_fe
     }
 }
 
+namespace std
+{
+    template <>
+    struct hash< libcasm_fe::Ast::FunctionDefinition::UID >
+    {
+        std::size_t operator()( libcasm_fe::Ast::FunctionDefinition::UID uid ) const
+        {
+            return static_cast< std::size_t >( uid );
+        }
+    };
+}
+
 #endif // _LIB_CASMFE_DEFINITION_H_
 
 //
