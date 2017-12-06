616f55874a2ab558ca6a682b5e0d3dd784490881
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2017-12-06 03:38:57 +0100

    SymbolResolverPass: single agent constant

--- src/analyze/SymbolResolverPass.cpp
+++ src/analyze/SymbolResolverPass.cpp
@@ -293,6 +293,7 @@ void SymbolResolveVisitor::visit( DirectCallExpression& node )
     {
         static const auto SELF( "self" );
         static const auto AGENT( "Agent" );
+        static const auto SINGLE_AGENT_CONSTANT( "$" );
 
         if( name == SELF )
         {
@@ -309,19 +310,20 @@ void SymbolResolveVisitor::visit( DirectCallExpression& node )
         }
         // single agent execution notation --> agent type domain ==
         // Enumeration!
-        else if( name == "$" )
+        else if( name == SINGLE_AGENT_CONSTANT )
         {
             assert( node.targetType() == CallExpression::TargetType::CONSTANT );
 
-            const auto agent =
-                std::make_shared< EnumeratorDefinition >( std::make_shared< Identifier >( "$" ) );
+            const auto agent = std::make_shared< EnumeratorDefinition >(
+                std::make_shared< Identifier >( SINGLE_AGENT_CONSTANT ) );
             const auto agentEnumerators = std::make_shared< Enumerators >();
             agentEnumerators->add( agent );
             const auto agentEnum = std::make_shared< EnumerationDefinition >(
                 std::make_shared< Identifier >( AGENT ), agentEnumerators );
 
-            const auto kind = libstdhl::Memory::make< libcasm_ir::Enumeration >( AGENT );
-            kind->add( "$" );
+            const auto kind
+                = libstdhl::Memory::make< libcasm_ir::Enumeration >( AGENT );
+            kind->add( SINGLE_AGENT_CONSTANT );
 
             const auto type = libstdhl::Memory::make< libcasm_ir::EnumerationType >( kind );
             agent->setType( type );
