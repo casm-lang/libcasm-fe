a44b207e1bd3575e76507c46a7bcd6c0b37f1c30
Philipp Paulweber <p.paulweber@gmail.com>
2017-12-16 15:32:09 +0100

    PropertyResolverPass: added first recursive visitor implementation

--- src/analyze/PropertyResolverPass.cpp
+++ src/analyze/PropertyResolverPass.cpp
@@ -68,6 +68,58 @@ class PropertyResolverVisitor final : public RecursiveVisitor
   public:
     PropertyResolverVisitor( libcasm_fe::Logger& log );
 
+    void visit( Specification& specification );
+
+    void visit( VariableDefinition& node ) override;
+    void visit( FunctionDefinition& node ) override;
+    void visit( DerivedDefinition& node ) override;
+    void visit( RuleDefinition& node ) override;
+    void visit( EnumeratorDefinition& node ) override;
+    void visit( EnumerationDefinition& node ) override;
+
+    void visit( TypeCastingExpression& node ) override;
+    void visit( ValueAtom& node ) override;
+    void visit( ReferenceAtom& node ) override;
+    void visit( UndefAtom& node ) override;
+    void visit( DirectCallExpression& node ) override;
+    void visit( MethodCallExpression& node ) override;
+    void visit( IndirectCallExpression& node ) override;
+    void visit( UnaryExpression& node ) override;
+    void visit( BinaryExpression& node ) override;
+    void visit( RangeExpression& node ) override;
+    void visit( ListExpression& node ) override;
+    void visit( LetExpression& node ) override;
+    void visit( ConditionalExpression& node ) override;
+    void visit( ChooseExpression& node ) override;
+    void visit( UniversalQuantifierExpression& node ) override;
+    void visit( ExistentialQuantifierExpression& node ) override;
+
+    void visit( SkipRule& node ) override;
+    void visit( ConditionalRule& node ) override;
+    void visit( CaseRule& node ) override;
+    void visit( LetRule& node ) override;
+    void visit( ForallRule& node ) override;
+    void visit( ChooseRule& node ) override;
+    void visit( IterateRule& node ) override;
+    void visit( BlockRule& node ) override;
+    void visit( SequenceRule& node ) override;
+    void visit( UpdateRule& node ) override;
+    void visit( CallRule& node ) override;
+
+    void visit( UnresolvedType& node ) override;
+    void visit( BasicType& node ) override;
+    void visit( ComposedType& node ) override;
+    void visit( FixedSizedType& node ) override;
+    void visit( RelationType& node ) override;
+
+    void visit( BasicAttribute& node ) override;
+    void visit( ExpressionAttribute& node ) override;
+
+    void visit( Identifier& node ) override;
+    void visit( IdentifierPath& node ) override;
+    void visit( ExpressionCase& node ) override;
+    void visit( DefaultCase& node ) override;
+
   private:
     libcasm_fe::Logger& m_log;
 };
@@ -75,6 +127,234 @@ class PropertyResolverVisitor final : public RecursiveVisitor
 PropertyResolverVisitor::PropertyResolverVisitor( libcasm_fe::Logger& log )
 : m_log( log )
 {
+}
+
+void PropertyResolverVisitor::visit( Specification& specification )
+{
+    specification.header()->accept( *this );
+    specification.definitions()->accept( *this );
+}
+
+void PropertyResolverVisitor::visit( VariableDefinition& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( FunctionDefinition& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( DerivedDefinition& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( RuleDefinition& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( EnumeratorDefinition& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( EnumerationDefinition& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( TypeCastingExpression& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( ValueAtom& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( ReferenceAtom& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( UndefAtom& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( DirectCallExpression& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( MethodCallExpression& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( IndirectCallExpression& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( UnaryExpression& node )
+{
+    RecursiveVisitor::visit( node );
+    node.setProperties( node.expression()->properties() );
+}
+
+void PropertyResolverVisitor::visit( BinaryExpression& node )
+{
+    RecursiveVisitor::visit( node );
+    node.setProperties( node.left()->properties() | node.right()->properties() );
+}
+
+void PropertyResolverVisitor::visit( RangeExpression& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( ListExpression& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( LetExpression& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( ConditionalExpression& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( ChooseExpression& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( UniversalQuantifierExpression& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( ExistentialQuantifierExpression& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( SkipRule& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( ConditionalRule& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( CaseRule& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( LetRule& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( ForallRule& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( ChooseRule& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( IterateRule& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( BlockRule& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( SequenceRule& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( UpdateRule& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( CallRule& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( UnresolvedType& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( BasicType& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( ComposedType& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( FixedSizedType& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( RelationType& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( BasicAttribute& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( ExpressionAttribute& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( Identifier& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( IdentifierPath& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( ExpressionCase& node )
+{
+    RecursiveVisitor::visit( node );
+}
+
+void PropertyResolverVisitor::visit( DefaultCase& node )
+{
+    RecursiveVisitor::visit( node );
 }
 
 void PropertyResolverPass::usage( libpass::PassUsage& pu )
